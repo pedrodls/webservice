703bd33eb4dcbaadf2ed1f70bca68de23dad1f01
PedroSousa2020 <sousapedro747@gmail.com>
2020-06-23 22:21:27 +0100

    chat em construcao

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -20,11 +20,13 @@ namespace ChatService
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Content/fontawesome-free-5.13.1-web/js/all.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css", 
+                      "~/Content/fontawesome-free-5.13.1-web/css/all.css"));
         }
     }
 }
