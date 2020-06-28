4898d6cadf126acca605e3e61e8cd2a86b002ff5
PedroSousa2020 <sousapedro747@gmail.com>
2020-06-28 17:21:39 +0100

    Criado o chat e recolha de dados em js

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace ChatService
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js", "~/Scripts/app/recolha_dados.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
