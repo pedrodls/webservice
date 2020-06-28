454adb0ff48fa67eee328061df10178582a0820b
PedroSousa2020 <sousapedro747@gmail.com>
2020-06-28 17:26:54 +0100

    ficheiros js adicionados

---
+++ Scripts/app/recolha_dados.js
@@ -0,0 +1,26 @@
+﻿var vetObj = [{id:0}];
+
+var btnSbt = $("#send");
+
+btnSbt.on('click', function () {
+
+    var obj = { text: $("#message").val() };
+
+    addObj(obj);
+
+});
+
+function addObj(obj) {
+
+    obj.date = new Date();
+    obj.id = vetObj[vetObj.length - 1].id + 1;
+    obj.source = true;
+
+    alert(obj.text);
+
+    vetObj.push(obj);
+}
+
+
+
+
